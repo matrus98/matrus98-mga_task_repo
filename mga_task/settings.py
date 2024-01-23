042a0a0efca3ab3f01046c7b5b1a0567e3560f81
matrus98 <mati.ruszkowski@wp.pl>
2024-01-23 16:22:23 +0100

    Database change
    Database for application has been replaced from sqlite (default in Django) to Postgres working in docker container, which might be run with docker compose file.

--- mga_task/settings.py
+++ mga_task/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mga_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'PASSWORD': 'mysecretpassword',
     }
 }
 
