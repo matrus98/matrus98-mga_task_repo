7822610847dc29e62a99f991fc59fba1ba339b10
matrus98 <mati.ruszkowski@wp.pl>
2024-02-12 22:10:22 +0100

    css fix

--- mga_task/settings.py
+++ mga_task/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "task_manager/static/",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
